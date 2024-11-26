24c07959bf0f2179f2ad15bade9fdbc8d4574de3
Prateek Kharwar <kharwarp0202@gmail.com>
2024-11-26 09:01:44 +0530

    Connecting wallet

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+
+import { LotterContext } from '../../context/LotterContext';
+
 const Navbar = () => {
+
+    const { connectWallet, currentAccount } = useContext(LotterContext);
+
     return (
         <div className="navbar">
             <nav>
@@ -13,7 +19,7 @@ const Navbar = () => {
             </div>
             <nav>
                 <Link to="/">About Me</Link>
-                <Link to="/">Contact</Link>
+                <button id="connect-wallet" onClick={connectWallet} >{!currentAccount ? 'Connect Wallet' : 'Connected'}</button>
             </nav>
         </div>
     );
--- src/context/LotterContext.jsx
+++ src/context/LotterContext.jsx
@@ -6,12 +6,12 @@ import { contractABI, contractAddress } from "../utils/constants";
 
 export const LotterContext = React.createContext();
 
-const { etherium } = window;
+const { ethereum } = window;
 
 const getEthereumContract = () => {
-    const provider = ethers.providers.Web3Provider(etherium);
+    const provider = ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
-    const lotterContract = new etherium.Contract(contractAddress, contractABI, signer);
+    const lotterContract = new ethereum.Contract(contractAddress, contractABI, signer);
     console.log({
         provider,
         signer,
@@ -20,8 +20,47 @@ const getEthereumContract = () => {
 }
 
 export const LotterProvider = ({ children }) => {
+
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const checkIfWalletIsConnected = async () => {
+
+        try {
+            if(!ethereum) return alert("Please install metamask");
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+            if(accounts.length){
+                setCurrentAccount(accounts[0]);
+                //getALL
+            }else{
+                console.log("No account Found");
+            }
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.")
+        }
+
+    }
+
+    const connectWallet = async () => {
+        try {
+            if(!ethereum) return alert("Please install metamask");
+
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            setCurrentAccount(accounts[0]);
+
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.")
+        }
+    }
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+
     return (
-        <LotterContext.Provider value={{ value: 'test' }}>
+        <LotterContext.Provider value={{ connectWallet, currentAccount }}>
             { children }
         </LotterContext.Provider>
     );
